09e7e9b86514267af4a3165ce525bbb65d5e0164
victorucama-create <victorucama@gmail.com>
2025-11-19 10:28:07 +0200

    Create auth.middleware.js

---
+++ backend/src/middleware/auth.middleware.js
@@ -0,0 +1,37 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User.model");
+
+async function authMiddleware(req, res, next) {
+  try {
+    const token = req.headers.authorization?.split(" ")[1];
+
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        message: "Token missing",
+      });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await User.findById(decoded.id);
+
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid token",
+      });
+    }
+
+    req.user = user;
+
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      success: false,
+      message: "Unauthorized",
+    });
+  }
+}
+
+module.exports = { authMiddleware };
