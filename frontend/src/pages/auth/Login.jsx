da93645a50339af1b8aa00e3c37e034086b00cec
victorucama-create <victorucama@gmail.com>
2025-11-20 02:50:35 +0200

    Update Login.jsx

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -1,3 +1,35 @@
+import { useState, useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
+
 export default function Login() {
-  return <div>Login Page</div>;
+  const { login } = useContext(AuthContext);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    await login(email, password);
+  }
+
+  return (
+    <div className="auth-container">
+      <h2>Entrar</h2>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Senha"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button type="submit">Entrar</button>
+      </form>
+    </div>
+  );
 }
