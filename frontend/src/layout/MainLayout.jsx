05da7c49c093c1b46f745f4af71614aa97315538
victorucama-create <victorucama@gmail.com>
2025-11-20 02:41:49 +0200

    Update MainLayout.jsx

--- frontend/src/layout/MainLayout.jsx
+++ frontend/src/layout/MainLayout.jsx
@@ -1,15 +1,18 @@
 import Sidebar from "./Sidebar";
 import Topbar from "./Topbar";
+import { Outlet } from "react-router-dom";
 import "./Layout.css";
 
-export default function MainLayout({ children }) {
+export default function MainLayout() {
   return (
     <div className="gn-layout">
       <Sidebar />
+
       <div className="gn-main-area">
         <Topbar />
+
         <div className="gn-page-content">
-          {children}
+          <Outlet />
         </div>
       </div>
     </div>
